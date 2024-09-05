6d08013ab10e4d2083874fa5221edad401a4ee65
forefy <tomer@auditware.io>
2024-09-05 13:30:00 +0300

    Capture exit code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl -L https://raw.githubusercontent.com/auditware/radar/main/install-radar.sh | bash
 
 path="$1"
@@ -8,4 +10,12 @@ if [ -n "$ignore" ]; then
   radar -p "$path" -o "$outfile" --ignore "$ignore"
 else
   radar -p "$path" -o "$outfile"
+fi
+
+radar_exit_code=$?
+if [ "$radar_exit_code" -ne 0 ]; then
+  echo "Radar failed with exit status $radar_exit_code"
+  exit $radar_exit_code
+else
+  echo "Radar completed successfully with exit code 0"
 fi
